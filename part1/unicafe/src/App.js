dc9237ce24f3ef35ba49d5adbddcb3578307cc1d
LezamaCybart <lezamacybartmatiasgerman@gmail.com>
2021-09-23 19:31:51 -0300

    unicafe step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,11 +11,17 @@ const Button = ({handleClick, text}) => {
 const StatisticLine = ({text, value}) => {
     if (text === 'positive') { 
         return ( 
-            <p>{text} {value}%</p>
+            <tr>
+                <td>{text}</td>
+                <td>{value}%</td>
+            </tr>
         )
     }
     return ( 
-        <p>{text} {value}</p>
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
     )
 }
 
@@ -33,11 +39,15 @@ const Statistics = ({good, neutral, bad}) => {
     return ( 
       <div>
         <h1>statistics</h1>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive} />
+        <table>
+            <tbody>
+                <StatisticLine text="good" value={good} />
+                <StatisticLine text="neutral" value={neutral} />
+                <StatisticLine text="bad" value={bad} />
+                <StatisticLine text="average" value={average} />
+                <StatisticLine text="positive" value={positive} />
+            </tbody>
+        </table>
       </div>
     )
 }
